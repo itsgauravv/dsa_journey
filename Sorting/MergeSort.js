d0fb865b2892e35d14373591a7da76bf51665567
itsgauravv <gauravjani3113@gmail.com>
2025-01-18 12:49:33 +0530

    fix

--- Sorting/MergeSort.js
+++ Sorting/MergeSort.js
@@ -1,3 +1,4 @@
 // Merge Sort optimized alogorithm important
 // Time Complexity: O(n log n)
 // merge sort is a divide and conquer algorithm that was invented by John von Neumann in 1945.
+// It is a comparison-based algorithm that focuses on how to merge together two pre-sorted arrays such that the resulting array is also sorted.
